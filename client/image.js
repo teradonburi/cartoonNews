60c30151fe0d867db90715aa010d3c5da265f007
teradonburi <daikiterai@gmail.com>
2019-09-28 15:54:51 +0900

    レイアウト修正

---
+++ client/image.js
@@ -0,0 +1,11 @@
+
+
+export function loadImage(src) {
+  return new Promise((resolve) => {
+    const img = new Image()
+    img.onload = () => {
+      resolve(img)
+    }
+    img.src = src
+  })
+}
